11f55da81dd4da06f9bedecae04cfefc50338f9d
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2025-09-03 15:12:31 +0200

    added basic meta-test

--- tests/test_log.c
+++ tests/test_log.c
@@ -1,10 +1,36 @@
-// #define LOG_PRINTF demo_printf
+// mock the printf function
+#define mock_printf(format, ...) snprintf(mock_stdout, sizeof(mock_stdout), format, __VA_ARGS__);
+#define LOG_PRINTF mock_printf
 
+#include <string.h>
 #include "macrotools/test.h"
 #include "macrotools/log.h"
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+char mock_stdout[1024];
+
 DESCRIBE(test_log, "log") {
-    IT("prints errors") {
-        LOG_ERROR("Some message.");
+    IT("logs messages") {
+        int log_line;
+
+        snprintf(mock_stdout, sizeof(mock_stdout), "123");
+        // clang-format off
+        LOG_ERROR("Some message."); log_line = __LINE__;
+        // clang-format on
+
+        char expected_stdout[1024];
+        snprintf(expected_stdout, sizeof(expected_stdout),
+            MT_COLOR_RED "ERROR" MT_COLOR_RESET " %s:%d: Some message.\r\n",
+            __FILE__,
+            log_line);
+        EXPECT_EQ(strncmp(mock_stdout, expected_stdout, MIN(sizeof(mock_stdout), sizeof(expected_stdout))), 0);
+    }
+
+    IT("skips logging if the level is not enabled") {
+        snprintf(mock_stdout, sizeof(mock_stdout), "123");
+        LOG_TRACE("Some other message.");
+
+        EXPECT_EQ(strncmp(mock_stdout, "123", sizeof(mock_stdout)), 0);
     }
 }
